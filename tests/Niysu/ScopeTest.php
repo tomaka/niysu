d76885343f2c654c6edd4f68c337d80793546411
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-14 17:15:44 +0100

    Added some new tests

--- tests/Niysu/ScopeTest.php
+++ tests/Niysu/ScopeTest.php
@@ -11,13 +11,36 @@ class ScopeTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals($scope->getVariable('testB'), 2);
 		$this->assertEquals($scope->getVariable('testC'), 3);
 	}
+
+	public function testScopeAccessible() {
+		$scope = new Scope();
+		$scope->add('test', 1);
+		$scope->callFunction(function($scope) { $this->assertEquals($scope->getVariable('test', 1)); });
+	}
+
+	public function testClone() {
+		$scope1 = new Scope();
+		$scope1->add('test', 1);
+
+		$scope2 = clone $scope1;
+		$this->assertEquals($scope2->getVariable('test'), 1);
+	}
 	
+	public function testCallFunctionByType() {
+		$scope = new Scope();
+		$scope->add('e', new \LogicException('test'));
+		$scope->callFunction(function(\Exception $x) {
+			$this->assertNotNull($x);
+			$this->assertEquals($x->getMessage(), 'test');
+		});
+	}
+
 	public function testCallFunction() {
 		$scope = new Scope();
 		$scope->add('test', 1);
 		$scope->callFunction(function($test) { $this->assertEquals($test, 1); });
 	}
-	
+
 	public function testAddByCallback() {
 		$scope = new Scope();
 		$scope->addByCallback('test', function() { return 3; });
