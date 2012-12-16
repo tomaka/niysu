2f67e41be23b88563b958451d1fad96aa706bfec
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-16 14:06:59 +0100

    Added more Route tests

--- tests/Niysu/RouteTest.php
+++ tests/Niysu/RouteTest.php
@@ -30,17 +30,69 @@ class RouteTest extends \PHPUnit_Framework_TestCase {
 	public function testMethodMatch() {
 		$response = new HTTPResponseStorage();
 		$scope = new Scope([ 'response' => $response ]);
-
 		$route = new Route('/', 'test', function() {});
 
 		$scope->request = new HTTPRequestCustom('/', 'get');
 		$this->assertFalse($route->handle($scope));
-
 		$scope->request = new HTTPRequestCustom('/', 'post');
 		$this->assertFalse($route->handle($scope));
-
 		$scope->request = new HTTPRequestCustom('/', 'test');
 		$this->assertTrue($route->handle($scope));
+	}
+
+	public function testAnyMethodMatch() {
+		$this->markTestIncomplete();
+
+		$response = new HTTPResponseStorage();
+		$scope = new Scope([ 'response' => $response ]);
+		$route = new Route('/', '*', function() {});
+
+		$scope->request = new HTTPRequestCustom('/', 'get');
+		$this->assertTrue($route->handle($scope));
+		$scope->request = new HTTPRequestCustom('/', 'post');
+		$this->assertTrue($route->handle($scope));
+		$scope->request = new HTTPRequestCustom('/', 'test-test/test@test');
+		$this->assertTrue($route->handle($scope));
+	}
+	
+	public function testPatternFunction() {
+		$response = new HTTPResponseStorage();
+		$scope = new Scope([ 'response' => $response ]);
+		$route = new Route('/{var}', 'get', function() {});
+		$route->pattern('var', '\\d+');
+		
+		$scope->request = new HTTPRequestCustom('/', 'post');
+		$this->assertFalse($route->handle($scope));
+		$scope->request = new HTTPRequestCustom('/3', 'get');
+		$this->assertTrue($route->handle($scope));
+		$scope->request = new HTTPRequestCustom('/3x', 'post');
+		$this->assertFalse($route->handle($scope));
+		$scope->request = new HTTPRequestCustom('/x', 'test');
+		$this->assertFalse($route->handle($scope));
+	}
+	
+	/**
+     * @depends testPatternFunction
+     */
+	public function testParametersPassing() {
+		$response = new HTTPResponseStorage();
+		$scope = new Scope([ 'response' => $response, 'externalVariable' => 'externalVariableValue' ]);
+
+		$route = new Route('/{var1}{var2}/{var3}', 'get', function($var1, $var2, $var3, $externalVariableValue, &$valueByRef) {
+			$valueByRef = 18;
+			$this->assertEquals($var1, 2);
+			$this->assertEquals($var2, 'hello');
+			$this->assertEquals($var3, 'goodbye');
+		});
+		$route->pattern('var1', '\\d+');
+		
+		$scope->request = new HTTPRequestCustom('/2hello/goodbye');
+		$this->assertTrue($route->handle($scope));
+
+		$this->assertEquals($scope->var1 2);
+		$this->assertEquals($scope->var2, 'hello');
+		$this->assertEquals($scope->var3, 'goodbye');
+		$this->assertEquals($scope->valueByRef, 18);
 	}
 };
 
