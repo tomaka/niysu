fa90a067d2b4e9ad6c9b4627d5e233ea17d543f6
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-16 12:27:58 +0100

    Added HTTPResponseStorage

---
+++ src/Niysu/HTTPResponseStorage.php
@@ -0,0 +1,56 @@
+<?php
+namespace Niysu;
+
+class HTTPResponseStorage {
+	public function getStatusCode() {
+		return $this->statusCode;
+	}
+
+	public function setStatusCode($statusCode) {
+		$this->statusCode = $statusCode;
+	}
+	
+	public function hasHeader($header) {
+		return isset($this->headers[$header]);
+	}
+	
+	public function getHeader($header) {
+		return $this->headers[$header];
+	}
+	
+	public function getHeadersList() {
+		return $this->headers;
+	}
+	
+	public function addHeader($header, $value) {
+		// TODO:
+		$this->setHeader($header, $value);
+	}
+
+	public function setHeader($header, $value) {
+		$this->headers[$header] = $value;
+	}
+
+	public function removeHeader($header) {
+		unset($this->headers[$header]);
+	}
+
+	public function isHeadersListSent() {
+		return false;
+	}
+
+	public function getData() {
+		return $this->data;
+	}
+	
+	public function appendData($data) {
+		$this->data .= $data;
+	}
+
+
+	private $statusCode = 200;
+	private $headers = [];
+	private $data = '';
+};
+
+?>
---
+++ tests/Niysu/HTTPResponseStorageTest.php
@@ -0,0 +1,51 @@
+<?php
+namespace Niysu;
+
+class HTTPResponseStorageTest extends \PHPUnit_Framework_TestCase {
+	public function testAppendData() {
+		$response = new HTTPResponseStorage();
+		$this->assertEquals($response->getData(), '');
+
+		$response->appendData('ABC');
+		$this->assertEquals($response->getData(), 'ABC');
+
+		$response->appendData('DEF');
+		$this->assertEquals($response->getData(), 'ABCDEF');
+	}
+
+	public function testSetStatusCode() {
+		$response = new HTTPResponseStorage();
+		$this->assertGreaterThanOrEqual($response->getStatusCode(), 200);
+		$this->assertLessThan($response->getStatusCode(), 300);
+		
+		$response->setStatusCode(301);
+		$this->assertEquals($response->getStatusCode(), 301);
+		
+		$response->setStatusCode(404);
+		$this->assertEquals($response->getStatusCode(), 404);
+		
+		$response->setStatusCode(500);
+		$this->assertEquals($response->getStatusCode(), 500);
+	}
+
+	public function testSetHeader() {
+		$response = new HTTPResponseStorage();
+		$this->assertFalse($response->hasHeader('Content-Type'));
+		$response->setHeader('Content-Type', 'text/html');
+		$this->assertTrue($response->hasHeader('Content-Type'));
+		$this->assertEquals($response->getHeader('Content-Type'), 'text/html');
+	}
+
+	public function testRemoveHeader() {
+		$response = new HTTPResponseStorage();
+		$this->assertFalse($response->hasHeader('Content-Type'));
+		
+		$response->setHeader('Content-Type', 'text/html');
+		$this->assertTrue($response->hasHeader('Content-Type'));
+		
+		$response->removeHeader('Content-Type');
+		$this->assertFalse($response->hasHeader('Content-Type'));
+	}
+};
+
+?>
