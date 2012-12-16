e477423d7cef6b7c27679475dedc168a234c1ed1
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-16 17:58:13 +0100

    Added check for X-StatusOnLoginFail to be between 400 and 500

--- src/Niysu/Services/HTTPBasicAuthService.php
+++ src/Niysu/Services/HTTPBasicAuthService.php
@@ -4,9 +4,9 @@ namespace Niysu\Services;
 class HTTPBasicAuthService {
 	public static function beforeMustBeAuthenticated($realm = 'private') {
 		return function($httpBasicAuthService, $request, $response, &$callHandler) use ($realm) {
-			$statusOnFail = 401;
-			if ($request->getHeader('X-StatusOnLoginFail'))
-				$statusOnFail = intval($request->getHeader('X-StatusOnLoginFail'));
+			$statusOnFail = $request->getHeader('X-StatusOnLoginFail');
+			if (!$statusOnFail || $statusOnFail < 400 || $statusOnFail >= 500)
+				$statusOnFail = 401;
 
 			if ($httpBasicAuthService->login())
 				return;
