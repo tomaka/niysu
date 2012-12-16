810dbf1426fd68db228da387d3a5f1629bf943d4
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-16 14:12:41 +0100

    Added tests to DatabaseServiceTest

--- tests/Niysu/DatabaseServiceTest.php
+++ tests/Niysu/DatabaseServiceTest.php
@@ -21,6 +21,21 @@ class DatabaseServiceTest extends \PHPUnit_Framework_TestCase {
 		$val = $this->service->test[0]->id();
 		$this->assertEquals($val, 1);
 	}
+	
+	/**
+     * @depends testSelect
+     */
+	public function testForeach() {
+		$this->service->execute('INSERT INTO test VALUES (1)');
+		$this->service->execute('INSERT INTO test VALUES (2)');
+		$this->service->execute('INSERT INTO test VALUES (3)');
+
+		$i = 1;
+		foreach ($this->service->test as $element) {
+			$this->assertEquals($element->id(), $i);
+			++$i;
+		}
+	}
 
 	/**
      * @depends testSelect
@@ -30,20 +45,26 @@ class DatabaseServiceTest extends \PHPUnit_Framework_TestCase {
 		$val = $this->service->test[0]->id();
 		$this->assertEquals($val, 2);
 	}
-	
+
 	/**
-     * @depends testSelect
+     * @depends testInsert
      */
-	public function testForeach() {
+	public function testMultipleInsert() {
+		$this->markTestIncomplete();
+
+		$this->service->test[] = [ [ 'id' => 2 ], [ 'id' => 3 ], [ 'id' => 4 ], [ 'id' => 5 ] ];
+		$this->assertEquals(count($this->service->test, 4));
+		$this->assertEquals($this->service->test[0]->id(), 2);
+		$this->assertEquals($this->service->test[1]->id(), 3);
+		$this->assertEquals($this->service->test[2]->id(), 4);
+		$this->assertEquals($this->service->test[3]->id(), 5);
+	}
+	
+	public function testUpdate() {
 		$this->service->execute('INSERT INTO test VALUES (1)');
-		$this->service->execute('INSERT INTO test VALUES (2)');
-		$this->service->execute('INSERT INTO test VALUES (3)');
 		
-		$i = 1;
-		foreach ($this->service->test as $element) {
-			$this->assertEquals($element->id(), $i);
-			++$i;
-		}
+		$this->service->test->id = 5;
+		$this->assertEquals($this->service->test[0]->id(), 5);
 	}
 };
 
