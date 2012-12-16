53290147b9c5c20cc1450eb842f3fca74416c9ce
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-16 16:10:40 +0100

    Added DebugPanelService

--- src/Niysu/Services/DebugPanelService.php
+++ src/Niysu/Services/DebugPanelService.php
@@ -2,15 +2,25 @@
 namespace Niysu\Services;
 
 class DebugPanelService {
-	public function __construct(&$response) {
+	public function __construct(&$response, $scope) {
 		if (!$response)
 			throw new \LogicException('DebugPanelService can\'t be used outside of a route');
 
-		$response = new \Niysu\HTTPResponseCustomFilter($response, function($content) {
+		$response = new \Niysu\HTTPResponseCustomFilter($response, function($data) use ($scope) {
 			if (!$this->active)
-				return $content;
-			
-			if ()
+				return;
+			if (substr($data->getHeader('Content-Type'), 0, 9) != 'text/html')
+				return;
+
+			if (preg_match('/\\<\\/body\\>/i', $data->getData(), $matches, PREG_OFFSET_CAPTURE)) {
+				$timeElapsed = call_user_func($scope->elapsedTime);
+				$dbConnectionTime = 'unknown';
+				eval('$evaluatedPanel = "'.addslashes(self::$panelTemplate).'";');
+
+				$splitOffset = $matches[0][1];
+				$newContent = substr($data->getData(), 0, $splitOffset).$evaluatedPanel.substr($data->getData(), $splitOffset);
+				$data->setData($newContent);
+			}
 		});
 	}
 
@@ -23,7 +33,12 @@ class DebugPanelService {
 	}
 
 
-	private $active;
+	private $active = false;
+
+	private static $panelTemplate =
+		'<div style="position:fixed; left:0; bottom:0; width:100%; padding:0.5em 1em; background-color:gray; border-top:2px double black;">
+			$timeElapsed ms - $dbConnectionTime ms
+		</div>';
 };
 
 ?>
