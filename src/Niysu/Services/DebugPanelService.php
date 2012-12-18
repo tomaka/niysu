2eb9130ebef7e1c6cadff9ec4989953d6b8e2b7f
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-18 16:21:03 +0100

    Added database profiling in DebugPanel

--- src/Niysu/Services/DebugPanelService.php
+++ src/Niysu/Services/DebugPanelService.php
@@ -8,7 +8,9 @@ class DebugPanelService {
 		};
 	}
 
-	public function __construct(&$response, $scope) {
+	public function __construct(&$response, $scope, $databaseProfilingService) {
+		$this->databaseProfilingService = $databaseProfilingService;
+
 		if (!$response)
 			throw new \LogicException('DebugPanelService can\'t be used outside of a route');
 
@@ -21,6 +23,9 @@ class DebugPanelService {
 			if (preg_match('/\\<\\/body\\>/i', $data->getData(), $matches, PREG_OFFSET_CAPTURE)) {
 				$timeElapsed = call_user_func($scope->elapsedTime);
 				$peakMemory = self::formatBytes(memory_get_peak_usage());
+				$numQueries = $this->databaseProfilingService->getNumberOfQueries();
+				$queriesTime = $this->databaseProfilingService->getQueriesTotalMilliseconds();
+				$connectionMS = $this->databaseProfilingService->getTotalConnectionMilliseconds();
 				eval('$evaluatedPanel = "'.addslashes(self::$panelTemplate).'";');
 
 				$splitOffset = $matches[0][1];
@@ -53,12 +58,16 @@ class DebugPanelService {
 	
 	
 	private $active = false;
+	private $databaseProfilingService = null;
 	
 	private static $panelTemplate =
 		'<div style="color:black; position:fixed; left:0; bottom:0; width:100%; padding:0.5em 1em; background-color:gray; border-top:3px double black;">
 			<em><a style="color:darkblue; text-decoration:inherit;" href="https://github.com/Tomaka17/niysu">Niysu debug panel</a></em>
 			<span style="margin-left:2em;">Time to build this page: $timeElapsed ms</span>
 			<span style="margin-left:2em;">Peak memory: $peakMemory</span>
+			<span style="margin-left:2em;">Nb queries: $numQueries</span>
+			<span style="margin-left:2em;">Queries: $queriesTime ms</span>
+			<span style="margin-left:2em;">Connection: $connectionMS ms</span>
 		</div>';
 };
 
