c0e46354f3ed25411df60b924ac7b9c29e662f5e
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-18 20:38:04 +0100

    Content-type set to text/html in TwigService

--- src/Niysu/Services/TwigService.php
+++ src/Niysu/Services/TwigService.php
@@ -11,7 +11,7 @@ class TwigService {
 		$template = $this->twig->loadTemplate($template);
 		$output = $template->render($variables);
 		
-		//$this->response->setHeader('Content-Type', 'text/html; charset=utf8');
+		$this->response->setHeader('Content-Type', 'text/html; charset=utf8');
 		$this->response->appendData($output);
 	}
 
