d2fc46ca09e63b9244b6853feeb00226a5f092b1
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-19 07:04:45 +0100

    CookiesService now working properly

--- src/Niysu/Services/CookiesService.php
+++ src/Niysu/Services/CookiesService.php
@@ -2,9 +2,11 @@
 namespace Niysu\Services;
 
 class CookiesService {
-	public function __construct(&$request, &$response) {
+	public function __construct(&$request, &$response, &$logService) {
 		$this->request =& $request;
 		$this->response =& $response;
+		$this->logService =& $logService;
+		$this->refreshRequestCookies();
 	}
 
 	public function setDefaultLifetime($ttl) {
@@ -28,33 +30,67 @@ class CookiesService {
 	}
 
 	public function __isset($varName) {
-		return $this->has($varName);
+		return $this->get($varName) !== null;
 	}
 
 	public function getCookiesList() {
-
-	}
-
-	public function has($cookieName) {
-		return isset($requestCookies[$cookieName]);
+		return array_merge($this->requestCookies, $this->updatedCookies);
 	}
 
 	public function get($cookieName) {
-		if (!isset($requestCookies[$cookieName]))
+		if (isset($this->updatedCookies[$cookieName]))
+			return $this->updatedCookies[$cookieName];
+
+		if (!isset($this->requestCookies[$cookieName]))
 			return null;
-		return $requestCookies[$cookieName];
+
+		return $this->requestCookies[$cookieName];
 	}
 
-	public function add($name, $value, $expires = null, $path = null, $domaine = null, $secure = false, $httponly = false) {
+	public function add($name, $value, $expires = null, $path = null, $domain = null, $secure = false, $httponly = false) {
+		if ($expires === null)
+			$expires = $this->defaultLifetime;
 
+		$this->updatedCookies[$name] = $value;
+
+		if (is_numeric($expires))
+			$expires = date('r', time() + intval($expires));
+
+		$header = $name.'='.$value;
+		if ($expires)	$header .= '; Expires='.$expires;
+		if ($path)		$header .= '; Path='.$path;
+		if ($domain)	$header .= '; Domain='.$domain;
+		if ($secure)	$header .= '; Secure';
+		if ($httponly)	$header .= '; HttpOnly';
+
+		if ($this->logService)
+			$this->logService->debug('Adding cookie '.$name.' set to value: '.$value);
+		if (!$this->request->isHTTPS() && $secure && $this->logService)
+			$this->logService->notice('Setting a secure cookie not through HTTPS is pointless');
+
+		$this->response->addHeader('Set-Cookie', $header);
+	}
+
+
+	private function refreshRequestCookies() {
+		$this->requestCookies = [];
+
+		$header = $this->request->getHeader('Cookie');
+		if (!$header) return;
+
+		foreach (explode(';', $header) as $cookie) {
+			list($name, $value) = explode('=', $cookie, 2);
+			$this->requestCookies[trim($name)] = trim($value);
+		}
 	}
 
 
 	private $request;
 	private $response;
-	private $requestCookies = [];		// cookies read from the request ; array of format name => value
-	private $cookies = [];				// each element of the array
-	private $defaultLifetime = 0;		// default lifetime for cookies when expires is null
+	private $logService;
+	private $requestCookies = [];			// cookies read from the request ; array of format name => value
+	private $updatedCookies = [];			// same format as $requestCookies but for cookies that have been set by this function
+	private $defaultLifetime = 0;			// default lifetime for cookies when expires is null
 };
 
 ?>
