14bce031ad88c3f1c69bfaec53dd43cc9b66e967
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-20 16:08:35 +0100

    Added documentation to HTTPResponseInterface

--- src/Niysu/HTTPResponseInterface.php
+++ src/Niysu/HTTPResponseInterface.php
@@ -3,33 +3,81 @@ namespace Niysu;
 
 /// \brief Interface for an HTTP response
 abstract class HTTPResponseInterface {
-	/// \brief Sets the status code to return with the headers
-	/// \pre !isHeadersListSent()
+	/**
+	 * Sets the status code to return with the headers.
+	 *
+	 * @pre !isHeadersListSent()
+	 * @param integer 	$statusCode 	The status code
+	 */
 	abstract public function setStatusCode($statusCode);
-	/// \brief Adds a header to the headers list
-	/// \pre !isHeadersListSent()
+
+	/**
+	 * Adds a header to the headers list.
+	 *
+	 * @pre !isHeadersListSent()
+	 * @param string 	$header 	Header to add
+	 * @param string 	$value 		Value of the header
+	 */
 	abstract public function addHeader($header, $value);
-	/// \brief Removes all headers with the same name and replaces them with the given header:value combo
-	/// \pre !isHeadersListSent()
+
+	/**
+	 * Removes all headers of this name and adds a new one.
+	 *
+	 * Equivalent to "removeHeader($header); addHeader($header, $value);"
+	 *
+	 * @pre !isHeadersListSent()
+	 * @param string 	$header 	Header to add
+	 * @param string 	$value 		Value of the header
+	 */
 	abstract public function setHeader($header, $value);
-	/// \brief Removes all headers with this name
-	/// \pre !isHeadersListSent()
+
+	/**
+	 * Removes all headers with this name.
+	 *
+	 * @pre !isHeadersListSent()
+	 * @param string 	$header 	Name of the headers to remove
+	 */
 	abstract public function removeHeader($header);
-	/// \brief Returns true if the headers list was already sent back to the web server
+
+	/**
+	 * Returns true if the response has already sent its headers and is now sending data.
+	 *
+	 * If true, then you can't modify headers anymore.
+	 *
+	 * @return boolean
+	 */
 	abstract public function isHeadersListSent();
-	/// \brief Adds content
-	/// \note Sends the headers list of not already sent
+
+	/**
+	 * Appends data to the end of the response.
+	 *
+	 * Note that this doesn't trigger a headers sending.
+	 *
+	 * @param string 	$data 		Data to append
+	 */
 	abstract public function appendData($data);
-	/// \brief Flushes the response
+
+	/**
+	 * Flushes the response.
+	 *
+	 * If some data has already been appended, then this will send all headers and the data already added.
+	 */
 	abstract public function flush();
-	
-	/// \brief
+
+
+	/**
+	 * Deprecated.
+	 * @deprecated
+	 */
 	public function redirect($target, $statusCode = 302) {
 		$this->setStatusCode($statusCode);
 		$this->setHeader('Location', $target);
 	}
-	
-	/// \param $type An array with keys like 'public' or 'max-age', with an optional value setting the value
+
+	/**
+	 * Deprecated.
+	 * @deprecated
+	 */
 	public function setCache($value) {
 		if (!is_array($value))
 			throw new \LogicException('$type must be an array');
@@ -49,13 +97,20 @@ abstract class HTTPResponseInterface {
 		$this->removeHeader('Expires');//.gmdate('D, d M Y H:i:s', time() + $expires).' GMT');
 		$this->setHeader('Cache-Control', $str);
 	}
-	
+
+	/**
+	 * Deprecated.
+	 * @deprecated
+	 */
 	public function setHTMLData($data) {
 		$this->setHeader('Content-Type', 'text/html');
 		$this->appendData($data);
 	}
 
-	/// \param $data A two-dimensionnal array
+	/**
+	 * Deprecated.
+	 * @deprecated
+	 */
 	public function setCSVData($data) {
 		$this->setHeader('Content-Type', 'text/csv; charset=utf8');
 
@@ -66,12 +121,20 @@ abstract class HTTPResponseInterface {
 		$this->appendData(stream_get_contents($fp));
 		fclose($fp);
 	}
-	
+
+	/**
+	 * Deprecated.
+	 * @deprecated
+	 */
 	public function setJSONData($data) {
 		$this->setHeader('Content-Type', 'application/json');
 		$this->appendData(json_encode($data));
 	}
 
+	/**
+	 * Deprecated.
+	 * @deprecated
+	 */
 	public function setXMLData($data) {
 		$this->setHeader('Content-Type', 'text/xml');
 
@@ -85,6 +148,10 @@ abstract class HTTPResponseInterface {
 		}
 	}
 
+	/**
+	 * Deprecated.
+	 * @deprecated
+	 */
 	public function setPlainTextData($data) {
 		$this->setHeader('Content-Type', 'text/plain; charset=utf8');
 		$this->appendData($data);
