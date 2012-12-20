d1ac8c4fd96b4c8dbf276c168e5dbbb11d983c75
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-20 20:56:55 +0100

    Added documentation to XSLTService

--- src/Niysu/Services/XSLTService.php
+++ src/Niysu/Services/XSLTService.php
@@ -1,6 +1,11 @@
 <?php
 namespace Niysu\Services;
 
+/**
+ * This class allows for easy XSLT transformations.
+ *
+ * It requires the php_xsl extension to run.
+ */
 class XSLTService {
 	public function __construct(&$response, $outputXMLService) {
 		if (!extension_loaded('xsl'))
@@ -10,6 +15,15 @@ class XSLTService {
 		$this->outputXMLService = $outputXMLService;
 	}
 	
+	/**
+	 * Transforms XML using XSLT.
+	 *
+	 * The $xml parameter can be either raw XML or an array that can be passed to OutputXMLService::toString.
+	 *
+	 * @param string 	$template 	Name of the file containing the XSLT template
+	 * @param mixed 	$xml 		The XML data to transform
+	 * @throws RuntimeException In case of a problem when parsing the XML, the XSLT, or during the transformation
+	 */
 	public function transform($template, $xml) {
 		if (is_array($xml)) {
 			$xmldoc = new \DOMDocument();
@@ -44,6 +58,15 @@ class XSLTService {
 		return $output;
 	}
 	
+	/**
+	 * Transforms XML using XSLT and writes it to the response.
+	 *
+	 * Same as transform, but outputs the data to the response. If no response is given, uses the response of the current route.
+	 *
+	 * @param string 					$template 	Name of the file containing the XSLT template
+	 * @param mixed 					$xml 		The XML data to transform
+	 * @param HTTPResponseInterface 	$response 	The response where to write the output, or null to use the response of the current route
+	 */
 	public function output($template, $xml, $response = null) {
 		if (!$response)
 			$response = $this->response;
