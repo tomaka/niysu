2cef814675ec7c6a5a6cda5ffea4de04d6c0ce72
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-20 15:42:30 +0100

    Documented OutputJSONService Added throw if output function called from outside a route

--- src/Niysu/Services/OutputJSONService.php
+++ src/Niysu/Services/OutputJSONService.php
@@ -1,17 +1,35 @@
 <?php
 namespace Niysu\Services;
 
-/// \brief 
+/**
+ * Allows to easily send JSON data to the response.
+ */
 class OutputJSONService {
 	public function __construct(\Niysu\Scope $scope) {
 		$this->scope = $scope;
 	}
 
+	/**
+	 * Turns a JSON object into a string.
+	 *
+	 * The implementation simply calls json_encode, see php.net
+	 *
+	 * @param mixed 	$json 		Data representing JSON
+	 * @return string
+	 */
 	public function toString($json) {
 		return json_encode($json);
 	}
 
+	/**
+	 * Turns a JSON object into a string and sends it to the response.
+	 *
+	 * @param mixed 	$json 		Data representing JSON
+	 */
 	public function output($json) {
+		if (!isset($this->scope->response))
+			throw new \LogicException('Cannot be called from outside a route');
+			
 		$this->scope->response->setHeader('Content-Type', 'application/json');
 		$this->scope->response->appendData($this->toString($json));
 	}
