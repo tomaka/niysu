776131ef0d6b5563405c9b3ec0480d272b1b261f
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-21 14:09:04 +0100

    Added URLPattern class

---
+++ src/Niysu/URLPattern.php
@@ -0,0 +1,126 @@
+<?php
+namespace Niysu;
+
+/**
+ * Allows to build patterns for URLs and check if a URL matches it.
+ */
+class URLPattern {
+	/**
+	 * @param string 	$pattern 		The URL pattern
+	 */
+	public function __construct($pattern) {
+		$this->originalPattern = $pattern;
+
+		$currentOffset = 0;
+		while (preg_match('/\{(\w+)\}/', $pattern, $match, PREG_OFFSET_CAPTURE, $currentOffset)) {
+			$matchBeginOffset = $match[0][1];
+			$varName = $match[1][0];
+
+			$this->patternRegex[] = str_replace('/', '\/', preg_quote(substr($pattern, $currentOffset, $matchBeginOffset - $currentOffset)));
+			$this->patternRegex[] = '(\w+)';
+
+			$this->variablesList[] = $varName;
+
+			$currentOffset = $matchBeginOffset + strlen($match[0][0]);
+		}
+		$this->patternRegex[] = str_replace('/', '\/', preg_quote(substr($pattern, $currentOffset)));
+
+		$this->patternRegex[0] = '/^'.$this->patternRegex[0];
+		$this->patternRegex[] = '$/';
+	}
+
+	/**
+	 * Checks whether the URL matches the pattern.
+	 *
+	 * Returns null if the URL doesn't match.
+	 * Otherwise, returns an array with variables => values.
+	 *
+	 * @param string 	$url 	The URL to test
+	 * @return mixed
+	 */
+	public function testURL($url) {
+		// checking whether the URL matches
+		if (!preg_match($this->getURLRegex(), $url, $matches))
+			return null;
+		
+		// extracting parts of the URL
+		$parameters = [];
+		for ($matchNum = 1, $varNum = 0; $matchNum < count($matches); ++$varNum) {
+			$varName = $this->variablesList[$varNum];
+			$parameters[$varName] = urldecode($matches[$matchNum]);
+			
+			// adding to matchNum the number of '(' in the regex part
+			$matchNum += count(explode('(', $this->patternRegex[$varNum * 2 + 1])) - 1;
+		}
+		return $parameters;
+	}
+
+	/**
+	 * Changes the pattern of a variable.
+	 *
+	 * @param string 	$varName 		The variable name
+	 * @param string 	$regex 			The regular expression (without / /)
+	 * @throws LogicException If the variable doesn't exist in the pattern
+	 */
+	public function pattern($varName, $regex) {
+		$pos = array_search($varName, $this->variablesList);
+		if ($pos === false)
+			throw new \LogicException('Variable doesn\'t exist in the pattern: '.$varName);
+
+		$this->patternRegex[$pos * 2 + 1] = '('.$regex.')';
+	}
+
+	/**
+	 * Returns the original pattern.
+	 *
+	 * @return string
+	 */
+	public function getOriginalPattern() {
+		return $this->originalPattern;
+	}
+
+	/**
+	 * Returns the regular expression to match with an URL.
+	 *
+	 * Includes / and / around the regex.
+	 *
+	 * @return string
+	 */
+	public function getURLRegex() {
+		return implode($this->patternRegex);
+	}
+
+	/**
+	 * Returns the URL of the route.
+	 *
+	 * @param array 	$parameters 	An associative array of parameter => value
+	 * @return string
+	 * @throws RuntimeException If some parameters are missing in the array
+	 * @throws RuntimeException If a parameter does not match the corresponding regex
+	 */
+	public function getURL($parameters = []) {
+		// cloning the pattern
+		$patternRegex = $this->patternRegex;
+
+		foreach ($this->patternRegexMatches as $offset => $varName) {
+			if (!isset($parameters[$varName]))
+				throw new \RuntimeException('Parameter missing in the array: '.$varName);
+
+			$val = $parameters[$varName];
+			if (!preg_match_all('/'.$patternRegex[$offset * 2].'/', $val))
+				throw new \RuntimeException('Parameter does not match its regex: '.$varName.' doesn\'t match '.$patternRegex[$offset * 2]);
+
+			$patternRegex[$offset * 2] = $val;
+		}
+		
+		return implode($patternRegex);
+	}
+
+
+
+	private $originalPattern;		// the raw URL passed to __construct
+	private $patternRegex = [];		// the different parts of the regex ; even offsets are constant parts ; uneven offsets are variable names
+	private $variablesList = [];	// the list of variables in the right order
+}
+
+?>
---
+++ tests/Niysu/URLPatternTest.php
@@ -0,0 +1,91 @@
+<?php
+namespace Niysu;
+
+class URLPatternTest extends \PHPUnit_Framework_TestCase {
+	/**
+	 */
+	public function testTestURLConstant() {
+		$pattern = new URLPattern('/test');
+		
+		$this->assertNotNull($pattern->testURL('/test'));
+		$this->assertNull($pattern->testURL('/tes'));
+		$this->assertNull($pattern->testURL('/testt'));
+		$this->assertNull($pattern->testURL('/'));
+		$this->assertNull($pattern->testURL('/test-test'));
+		$this->assertNull($pattern->testURL('/test/test'));
+	}
+
+	/**
+     * @depends testTestURLConstant
+     */
+	public function testTestURLSingle() {
+		$pattern = new URLPattern('/{var}');
+		
+		$this->assertNotNull($pattern->testURL('/test'));
+		$this->assertEquals(1, count($pattern->testURL('/test')));
+		$this->assertEquals('test', $pattern->testURL('/test')['var']);
+
+		$this->assertNotNull($pattern->testURL('/1'));
+		$this->assertEquals(1, $pattern->testURL('/1')['var']);
+
+		$this->assertNull($pattern->testURL('/'));
+		$this->assertNull($pattern->testURL('/test-test'));
+		$this->assertNull($pattern->testURL('/test/test'));
+	}
+
+	/**
+     * @depends testTestURLSingle
+     */
+	public function testPatternMatchMultiple() {
+		$pattern = new URLPattern('/{var1}/{var2}');
+
+		$this->assertNotNull($pattern->testURL('/test/test'));
+		$this->assertEquals(2, count($pattern->testURL('/test/test')));
+		$this->assertEquals('a', $pattern->testURL('/a/b')['var1']);
+		$this->assertEquals('b', $pattern->testURL('/a/b')['var2']);
+
+		$this->assertNull($pattern->testURL('/'));
+		$this->assertNull($pattern->testURL('//'));
+		$this->assertNull($pattern->testURL('/test'));
+		$this->assertNull($pattern->testURL('/test-test'));
+		$this->assertNull($pattern->testURL('/test/test/test'));
+	}
+
+	/**
+     * @expectedException LogicException
+     */
+	public function testPatternFunctionException() {
+		$pattern = new URLPattern('/{var}');
+		$pattern->pattern('test', '\\d+');
+	}
+
+	/**
+     * @depends testTestURLSingle
+     */
+	public function testPatternFunction() {
+		$pattern = new URLPattern('/{var}');
+		$pattern->pattern('var', '\\d+');
+
+		$this->assertNotNull($pattern->testURL('/187'));
+		$this->assertEquals(1, count($pattern->testURL('/187')));
+		$this->assertEquals(187, $pattern->testURL('/187')['var']);
+
+		$this->assertNull($pattern->testURL('/test'));
+		$this->assertNull($pattern->testURL('/1h2l1'));
+	}
+
+	/**
+     * @depends testPatternFunction
+     */
+	public function testParenthesisInPattern() {
+		$pattern = new URLPattern('/{var1}/{var2}');
+		$pattern->pattern('var1', '(\\d)+');
+
+		$this->assertNotNull($pattern->testURL('/187/test'));
+		$this->assertEquals(2, count($pattern->testURL('/187/test')));
+		$this->assertEquals(187, $pattern->testURL('/187/test')['var1']);
+		$this->assertEquals('test', $pattern->testURL('/187/test')['var2']);
+	}
+}
+
+?>
