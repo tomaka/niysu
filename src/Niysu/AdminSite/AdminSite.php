6c1cd5bd537333aefbf8414137845c3704fc716d
Tomaka17 <pierre.krieger1708@gmail.com>
2012-12-21 22:05:00 +0100

    Added basic administration site

---
+++ src/Niysu/AdminSite/AdminSite.php
@@ -0,0 +1,40 @@
+<?php
+namespace Niysu\AdminSite;
+
+/**
+ * 
+ * @copyright 	2012 Pierre Krieger <pierre.krieger1708@gmail.com>
+ * @license 	MIT http://opensource.org/licenses/MIT
+ * @link 		http://github.com/Tomaka17/niysu
+ *
+ * @static 		assets
+ */
+class AdminSite {
+	/**
+	 * @url /admin
+	 * @method GET
+	 */
+	public function mainPanel($twigService, $server) {
+		$routes = [];
+		foreach ($server->getRoutesList() as $r)
+			$routes[] = [ 'route' => $r->getOriginalPattern(), 'name' => $r->getName() ];
+
+		$twigService->addPath(__DIR__);
+		$twigService->output('home.adminsite.htm', [
+			'routes' => $routes
+		]);
+	}
+
+	/**
+	 * @url /admin/ajax-test
+	 * @method GET
+	 */
+	public function ajaxTestPanel($twigService) {
+		$twigService->addPath(__DIR__);
+		$twigService->output('ajaxTest.htm', [
+			'routes' => $routes
+		]);
+	}
+}
+
+?>
