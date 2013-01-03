294f3fe90da480c395704fa7f05165bf95b85a08
Tomaka17 <pierre.krieger1708@gmail.com>
2013-01-03 16:07:28 +0100

    Added documentation to SessionService

--- src/Niysu/Services/SessionService.php
+++ src/Niysu/Services/SessionService.php
@@ -11,20 +11,35 @@ class SessionService implements \ArrayAccess {
 	/**
 	 * Sets the category that will be passed to the CacheService.
 	 * By default, this is "sessions"
+	 * @param string 	$category 		The category in the CacheService
 	 */
 	public function setCategory($category) {
 		$this->category = $category;
 	}
 
+	/**
+	 * Sets the duration of session written with "offsetSet".
+	 * @param integer 		$duration 		Number of seconds until the session expires
+	 */
 	public function setSessionsDuration($duration) {
 		$this->ttl = $duration;
 	}
 
-
+	/**
+	 * Returns true if the session with this ID exists.
+	 * @param string 	$id 	Session ID
+	 * @return boolean
+	 */
 	public function offsetExists($id) {
 		return null !== $this->cacheService->load($id, $this->category);
 	}
 
+	/**
+	 * Returns the content of the session with this ID.
+	 * Returns an associative array with variables => values
+	 * @param string 	$id 	Session ID
+	 * @return array
+	 */
 	public function offsetGet($id) {
 		$data = $this->cacheService->load($id, $this->category);
 		if ($data === null)
@@ -32,6 +47,12 @@ class SessionService implements \ArrayAccess {
 		return unserialize($data);
 	}
 
+	/**
+	 * Sets the content of the session with this ID.
+	 * $value must be an associative array with variables => values
+	 * @param string 	$id 		Session ID
+	 * @param array 	$value 		Array containing the session variables
+	 */
 	public function offsetSet($id, $value) {
 		if (!is_array($value))
 			throw new \LogicException('Value must be an array');
@@ -40,10 +61,18 @@ class SessionService implements \ArrayAccess {
 		$this->cacheService->store($id, serialize($value), $this->ttl, $this->category);
 	}
 
+	/**
+	 * Deletes the session with this ID.
+	 * @param string 	$id 	Session ID
+	 */
 	public function offsetUnset($id) {
 		$this->cacheService->clear($id, $this->category);
 	}
 
+	/**
+	 * Constructor.
+	 * @param CacheService 		$cacheService 		The cache service to use
+	 */
 	public function __construct(CacheService $cacheService) {
 		$this->cacheService = $cacheService;
 	}
