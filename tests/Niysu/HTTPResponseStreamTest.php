c3d009f74016dc35604623e9816ae1377e0d0085
Tomaka17 <pierre.krieger1708@gmail.com>
2013-01-04 09:57:21 +0100

    Added more tests for HTTPResponseStream

--- tests/Niysu/HTTPResponseStreamTest.php
+++ tests/Niysu/HTTPResponseStreamTest.php
@@ -49,6 +49,27 @@ class HTTPResponseStreamTest extends \PHPUnit_Framework_TestCase {
 		$stream->fwrite('ABC');
 
 		$this->assertEquals(404, $response->getStatusCode());
+
+		$this->assertTrue($response->hasHeader('ETag'));
+		$this->assertEquals('test', $response->getHeader('ETag'));
+		$this->assertEquals('ABC', $response->getData());
+	}
+
+	/**
+	 * @depends testWriteStatusCode
+	 */
+	public function testWriteStatusHeader() {
+		$response = new HTTPResponseStorage();
+		$stream = HTTPResponseStream::build($response, true);
+
+		$stream->fwrite('HTTP/1.1 404 Not Found'."\r\n");
+		$stream->fwrite('ETag: test'."\r\n");
+		$stream->fwrite('Status: 301'."\r\n");
+		$stream->fwrite("\r\n");
+		$stream->fwrite('ABC');
+
+		$this->assertEquals(301, $response->getStatusCode());
+
 		$this->assertTrue($response->hasHeader('ETag'));
 		$this->assertEquals('test', $response->getHeader('ETag'));
 		$this->assertEquals('ABC', $response->getData());
@@ -63,6 +84,24 @@ class HTTPResponseStreamTest extends \PHPUnit_Framework_TestCase {
 		$stream->fwrite("\r\n");
 		$stream->fwrite('ABC');
 		
+		$this->assertEquals('ABC', $response->getData());
+	}
+
+	/**
+	 * @depends testWriteHeaders
+	 * @depends testWriteStatusHeader
+	 */
+	public function testWriteAllAtOnce() {
+		$response = new HTTPResponseStorage();
+		$stream = HTTPResponseStream::build($response, true);
+
+		$stream->fwrite('HTTP/1.1 302 See Other'."\r\n".'Content-Type: text/html'."\r\n".'ETag: test'."\r\n\r\n".'ABC');
+
+		$this->assertEquals(302, $response->getStatusCode());
+		$this->assertTrue($response->hasHeader('Content-Type'));
+		$this->assertEquals('text/html', $response->getHeader('Content-Type'));
+		$this->assertTrue($response->hasHeader('ETag'));
+		$this->assertEquals('test', $response->getHeader('ETag'));
 		$this->assertEquals('ABC', $response->getData());
 	}
 };
