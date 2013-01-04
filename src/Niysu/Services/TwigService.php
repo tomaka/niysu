e3a8ee7a96c2c3c21690bd7ba65664a3d569c45e
Tomaka17 <pierre.krieger1708@gmail.com>
2013-01-04 12:17:49 +0100

    Removed deprecated features from TwigService

--- src/Niysu/Services/TwigService.php
+++ src/Niysu/Services/TwigService.php
@@ -8,81 +8,45 @@ namespace Niysu\Services;
  * @link 		http://github.com/Tomaka17/niysu
  */
 class TwigService {
-	public function __construct(&$response) {
-		$this->response =& $response;
+	public function __construct() {
 		$this->filesystemLoader = new \Twig_Loader_Filesystem([]);
-	}
-
-	public function setCachePath($directory) {
-		if (!is_dir($directory))
-			throw new \LogicException('Cache directory for TwigService doesn\'t exist');
-		$this->cachePath = $directory;
-		$this->twig = null;
-	}
-
-	public function addPath($templateDir, $namespace = null) {
-		if ($namespace)		$this->filesystemLoader->addPath($templateDir, $namespace);
-		else				$this->filesystemLoader->addPath($templateDir);
-		$this->twig = null;
-	}
-
-	public function prependPath($templateDir, $namespace = null) {
-		if ($namespace)		$this->filesystemLoader->prependPath($templateDir, $namespace);
-		else				$this->filesystemLoader->prependPath($templateDir);
-		$this->twig = null;
-	}
-
-	public function addGlobal($variable, $value) {
-		$this->globals[$variable] = $value;
-		if ($this->twig)
-			$this->twig->addGlobal($variable, $value);
-	}
-
-	public function render($template, $variables = []) {
-		$this->buildTwig();
-		$template = $this->twig->loadTemplate($template);
-		return $template->render($variables);
-	}
-
-	/**
-	 * @deprecated Use TwigResponseFilter instead
-	 */
-	public function output($template, $variables = []) {
-		if (!$this->response)
-			throw new \LogicException('Response must be set to use the output function');
-
-		$this->buildTwig();
-		$template = $this->twig->loadTemplate($template);
-		$output = $template->render($variables);
-		
-		$this->response->setHeader('Content-Type', 'text/html; charset=utf8');
-		$this->response->appendData($output);
-	}
-
-
-
-	private function buildTwig() {
-		if ($this->twig)
-			return;
 
 		$loader = new \Twig_Loader_Chain([
 			$this->filesystemLoader,
 			new \Twig_Loader_String()
 		]);
 
-		$this->twig = new \Twig_Environment($loader, [
-			'cache' => $this->cachePath
-		]);
-
-		foreach($this->globals as $k => $v)
-			$this->twig->addGlobal($k, $v);
+		$this->twig = new \Twig_Environment($loader, []);
 	}
 
-	private $twig = null;
-	private $response = null;
+	public function setCachePath($directory) {
+		if (!is_dir($directory))
+			throw new \LogicException('Cache directory for TwigService doesn\'t exist');
+		$this->twig->setCache($directory);
+	}
+
+	public function addPath($templateDir, $namespace = null) {
+		if ($namespace)		$this->filesystemLoader->addPath($templateDir, $namespace);
+		else				$this->filesystemLoader->addPath($templateDir);
+	}
+
+	public function prependPath($templateDir, $namespace = null) {
+		if ($namespace)		$this->filesystemLoader->prependPath($templateDir, $namespace);
+		else				$this->filesystemLoader->prependPath($templateDir);
+	}
+
+	public function addGlobal($variable, $value) {
+		$this->twig->addGlobal($variable, $value);
+	}
+
+	public function render($template, $variables = []) {
+		$template = $this->twig->loadTemplate($template);
+		return $template->render($variables);
+	}
+
+
+	private $twig;
 	private $filesystemLoader;
-	private $globals = [];
-	private $cachePath = false;
 };
 
 ?>
