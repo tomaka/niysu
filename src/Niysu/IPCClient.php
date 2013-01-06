3157ec645366d92833b55e72769608db234f6413
Tomaka17 <pierre.krieger1708@gmail.com>
2013-01-06 22:35:57 +0100

    Added some documentation

--- src/Niysu/IPCClient.php
+++ src/Niysu/IPCClient.php
@@ -2,16 +2,34 @@
 namespace Niysu;
 
 /**
+ * Allows you to connect to an IPC server.
  *
  * @copyright 	2012 Pierre Krieger <pierre.krieger1708@gmail.com>
  * @license 	MIT http://opensource.org/licenses/MIT
  * @link 		http://github.com/Tomaka17/niysu
  */
 class IPCClient {
+	/**
+	 * Constructor.
+	 * Does nothing except configure the IPC server.
+	 * 
+	 * The address parameter must be a socket transport. See http://php.net/manual/en/transports.php
+	 * It must be the same as the value configured in the IPC server.
+	 *
+	 * @param string 	$address 	An address to pass to stream_socket_client
+	 */
 	public function __construct($address) {
 		$this->bindAddress = $address;
 	}
 
+	/**
+	 * Handles the request by calling the IPC server.
+	 *
+	 * This function will connect to the IPC server, send the request, and copy what the server sends back to the response.
+	 *
+	 * @param HTTPRequestInterface 		$httpRequest		The request to handle (if null, an instance of HTTPRequestGlobal)
+	 * @param HTTPResponseInterface 	$httpResponse		The response where to write the output (if null, an instance of HTTPResponseGlobal)
+	 */
 	public function handle(HTTPRequestInterface $httpRequest = null, HTTPResponseInterface $httpResponse = null) {
 		if (!$httpRequest)		$httpRequest = new HTTPRequestGlobal();
 		if (!$httpResponse)		$httpResponse = new HTTPResponseGlobal();
--- src/Niysu/IPCServer.php
+++ src/Niysu/IPCServer.php
@@ -2,17 +2,31 @@
 namespace Niysu;
 
 /**
+ * Creates a daemon that will listen for incoming connections and answer them.
  *
  * @copyright 	2012 Pierre Krieger <pierre.krieger1708@gmail.com>
  * @license 	MIT http://opensource.org/licenses/MIT
  * @link 		http://github.com/Tomaka17/niysu
  */
 class IPCServer {
+	/**
+	 * Constructor.
+	 * Does nothing except configure the IPC server.
+	 * 
+	 * The address parameter must be a socket transport. See http://php.net/manual/en/transports.php
+	 *
+	 * @param Server 	$server 	An instance of a Niysu server that will be used to handle the incoming requests
+	 * @param string 	$address 	An address to pass to stream_socket_server
+	 */
 	public function __construct(Server $server, $address) {
 		$this->niysuServer = $server;
 		$this->bindAddress = $address;
 	}
 
+	/**
+	 * Runs the server.
+	 * This function will block forever and never return, except if an exception is thrown during the startup of the server.
+	 */
 	public function run() {
 		if (ini_get('max_execution_time') != 0)
 			throw new \LogicException('PHP must have no max_execution_time in order to start a server');
