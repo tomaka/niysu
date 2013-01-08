12bd706b5fdd3d53502af7aec3f3849c326e7701
Tomaka17 <pierre.krieger1708@gmail.com>
2013-01-08 10:01:13 +0100

    Auth interface done

--- src/Niysu/Filters/HTTPBasicAuthFilter.php
+++ src/Niysu/Filters/HTTPBasicAuthFilter.php
@@ -7,66 +7,43 @@ namespace Niysu\Filters;
  * @license 	MIT http://opensource.org/licenses/MIT
  * @link 		http://github.com/Tomaka17/niysu
  */
-class HTTPBasicAuthFilter {
-	public static function beforeMustBeAuthenticated($realm = 'private') {
-		return function($httpBasicAuthService, $request, $response, &$stopRoute) use ($realm) {
-			$statusOnFail = $request->getHeader('X-StatusOnLoginFail');
-			if (!$statusOnFail || $statusOnFail < 400 || $statusOnFail >= 500)
-				$statusOnFail = 401;
-
-			if ($httpBasicAuthService->login())
-				return;
-			$response->setStatusCode($statusOnFail);
-			$response->setHeader('WWW-Authenticate', 'Basic realm="'.$realm.'"');
-			$stopRoute = true;
-		};
+class HTTPBasicAuthFilter extends \Niysu\HTTPRequestFilterInterface {
+	public function __construct(\Niysu\HTTPRequestInterface $request, \Niysu\AuthService $authService, \Monolog\Logger $log = null) {
+		parent::__construct($request);
+		$this->authService = $authService;
+		$this->log = $log;
 	}
 
-	public function __construct(&$request, $logService, $scope) {
-		$this->request = &$request;
-		$this->logService = $logService;
-		$this->scope = $scope;
-	}
-
-	public function setAuthFunction($callable) {
-		if (!is_callable($callable))
-			throw new \LogicException('Auth function must be callable');
-		$this->authFunction = $callable;
-	}
-
-	/// \ret False if the client didn't provide any username/password, or the result of calling the auth function
-	/// \todo Give access to parent scope (eg. merge function in Scope or something)
+	/**
+	 * @return False if the client didn't provide any username/password, or the result of calling the auth function
+	 */
 	public function login() {
-		//
-		if (!$this->request->isHTTPS() && $this->logService && strtolower($this->request->getHeader('X-Forwarded-Proto')) != 'https')
-			$this->logService->warn('HTTP basic authentication is discouraged if you don\'t use HTTPS');
-		if (!$this->request->getHeader('Authorization'))
+		if (!$this->getInput()->getHeader('Authorization'))
 			return false;
 
 		// getting login/password from headers
-		$authHeader = $this->request->getHeader('Authorization');
+		$authHeader = $this->getInput()->getHeader('Authorization');
 	    if (preg_match('/\s*Basic\s+(.*)$/i', $authHeader, $matches))
             list($login, $password) = explode(':', base64_decode($matches[1]));        	
         else
 			return false;
 
-		// checking
-		if (!$this->authFunction)
-			throw new \LogicException('Auth function has not been set');
+		if (!$login || !$password)
+			return false;
+
+		if ($this->log)
+			$this->log->debug('Login attempt through basic HTTP authentication');
+
+		// warning if not using HTTPS
+		if (!$this->getInput()->isHTTPS() && isset($this->log) && strtolower($this->getInput()->getHeader('X-Forwarded-Proto')) != 'https')
+			$this->log->notice('HTTP basic authentication is discouraged if you don\'t use HTTPS');
 
 		// calling auth function
-		$retValue = $this->scope->newChild(['login' => $login, 'password' => $password], true, false, false)->call($this->authFunction);
-
-		if ($this->logService)
-			$this->logService->info('Successful user login by basic HTTP authentication');
-
-		return $retValue;
+		return $this->authService->login([ 'login' => $login, 'password' => $password ]);
 	}
 
-	private $authFunction;				// callable function
-	private $request;
-	private $logService;
-	private $scope;
+	private $authService;
+	private $log;
 };
 
 ?>
--- src/Niysu/Services/AuthService.php
+++ src/Niysu/Services/AuthService.php
@@ -11,38 +11,82 @@ namespace Niysu\Services;
 class AuthService {
 	/**
 	 * Constructs the AuthService.
+	 *
+	 * @param Scope 	$scope 		The scope that will be accessible to callbacks
+	 * @param Logger 	$log 		Logging object
 	 */
-	public function __construct(\Monolog\Logger $log = null) {
+	public function __construct(\Niysu\Scope $scope = null, \Monolog\Logger $log = null) {
+		$this->scope = $scope;
+		if (!$scope)
+			$this->scope = new \Niysu\Scope();
+
 		$this->log = $log;
 	}
 
+	/**
+	 * Sets the callback to use when a login attempt is detected.
+	 * 
+	 * This callback will have access to the scope passed to the constructor.
+	 * It will also have access to the variables passed to the login() function, ie. usually "login" and "password".
+	 *
+	 * The callback should return an ID for the client, or something non-true in case of login failure.
+	 *
+	 * @param callable 		$callback 		Callback function
+	 */
 	public function setLoginCallback($callback) {
-
-	}
-
-	public function setAccessTestCallback($callback) {
-
+		$this->loginCallback = $callback;
 	}
 
 	/**
+	 * Sets the callback to use when an access request is made.
 	 * 
+	 * This callback will have access to the scope passed to the constructor.
+	 * It will also have access to the variables "userID" and "access", which are the parameters to test for.
+	 *
+	 * The callback should return true if the user has the specified access, or false if it has not.
+	 *
+	 * @param callable 		$callback 		Callback function
 	 */
-	public function login() {
-		
+	public function setAccessTestCallback($callback) {
+		$this->accessCallback = $callback;
+	}
+
+	/**
+	 * Login attempt of a client.
+	 *
+	 * Calls the login callback and returns what the login callback returns.
+	 * A non-true value (loose comparaison) means that the login failed.
+	 *
+	 * The parameter must be an associative array. The keys are not specified, but "login" and "password" are recommended if available.
+	 * The content of this array will be passed to the login function.
+	 * 
+	 * @param array 	$data 		See description
+	 * @return mixed
+	 */
+	public function login($data) {
+		return $this->scope->newChild($data, true, false, true)->call($this->loginCallback);
 	}
 
 	/**
 	 * Tests whether a user has a specific access.
+	 * 
 	 * This function calls the callback previously set with setAccessTestCallback.
-	 * @param string 	$userID 	Identifier of the user
+	 * 
+	 * It is recommended to use URI-like access, eg. "read:/users/1"
+	 * 
+	 * @param mixed 	$userID 	Identifier of the user
+	 * @param string 	$access 	The access to test
 	 * @return boolean
 	 */
 	public function hasAccess($userID, $access) {
-
+		return $this->scope->newChild([ 'userID' => $userID, 'access' => $access ], true, false, true)->call($this->loginCallback);
 	}
 
 
-	private $log = null;
+	private $scope;
+	private $loginCallback;
+	private $accessCallback;
+	private $log;
 };
 
 ?>
